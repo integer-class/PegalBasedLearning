5a1f70e56a3f195d48d83fbabe541b0fa0d2584f
Fiss <nicenicenice345@gmail.com>
2024-11-25 14:52:42 +0700

    fix: recording

--- expriview/lib/views/recording_page.dart
+++ expriview/lib/views/recording_page.dart
@@ -1,6 +1,5 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 class RecordingPage extends StatefulWidget {
   final String intervieweeName;
@@ -13,9 +12,6 @@ class RecordingPage extends StatefulWidget {
 
 class _RecordingPageState extends State<RecordingPage> {
   CameraController? _controller;
-  bool _isRecording = false;
-  Timer? _timer;
-  int _frameCount = 0;
 
   @override
   void initState() {
@@ -35,50 +31,14 @@ class _RecordingPageState extends State<RecordingPage> {
 
     try {
       await _controller!.initialize();
-      if (mounted) {
-        setState(() {});
-      }
+      if (mounted) setState(() {});
     } catch (e) {
       debugPrint('Error initializing camera: $e');
     }
   }
 
-  void _startRecording() {
-    setState(() {
-      _isRecording = true;
-      _frameCount = 0;
-    });
-
-    // Capture frame every 5 seconds
-    _timer = Timer.periodic(const Duration(seconds: 5), (timer) async {
-      if (_controller != null && _controller!.value.isInitialized) {
-        try {
-          final image = await _controller!.takePicture();
-          setState(() {
-            _frameCount++;
-          });
-          
-          // Here you would send the image to your API
-          debugPrint('Frame captured: ${image.path}');
-          // TODO: Implement API call here
-          
-        } catch (e) {
-          debugPrint('Error capturing frame: $e');
-        }
-      }
-    });
-  }
-
-  void _stopRecording() {
-    _timer?.cancel();
-    setState(() {
-      _isRecording = false;
-    });
-  }
-
   @override
   void dispose() {
-    _timer?.cancel();
     _controller?.dispose();
     super.dispose();
   }
@@ -94,47 +54,8 @@ class _RecordingPageState extends State<RecordingPage> {
     }
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Recording: ${widget.intervieweeName}'),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: AspectRatio(
-              aspectRatio: _controller!.value.aspectRatio,
-              child: CameraPreview(_controller!),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                if (_isRecording)
-                  Text(
-                    'Frames captured: $_frameCount',
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: _isRecording ? _stopRecording : _startRecording,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: _isRecording ? Colors.red : Colors.blue,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 32,
-                      vertical: 16,
-                    ),
-                  ),
-                  child: Text(
-                    _isRecording ? 'Stop Recording' : 'Start Recording',
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+      body: SafeArea(
+        child: CameraPreview(_controller!),
       ),
     );
   }
