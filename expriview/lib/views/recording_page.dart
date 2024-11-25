40882df09a967d08d44f5ea5ff351e0fa59285e5
Nathan <onethanggoro7@gmail.com>
2024-11-25 14:51:26 +0700

    fix: interview pages

--- expriview/lib/views/recording_page.dart
+++ expriview/lib/views/recording_page.dart
@@ -1,6 +1,4 @@
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 class RecordingPage extends StatefulWidget {
   final String intervieweeName;
@@ -12,101 +10,25 @@ class RecordingPage extends StatefulWidget {
 }
 
 class _RecordingPageState extends State<RecordingPage> {
-  CameraController? _controller;
   bool _isRecording = false;
-  Timer? _timer;
   int _frameCount = 0;
 
   @override
-  void initState() {
-    super.initState();
-    _initializeCamera();
-  }
-
-  Future<void> _initializeCamera() async {
-    final cameras = await availableCameras();
-    final firstCamera = cameras.first;
-
-    _controller = CameraController(
-      firstCamera,
-      ResolutionPreset.medium,
-      enableAudio: false,
-    );
-
-    try {
-      await _controller!.initialize();
-      if (mounted) {
-        setState(() {});
-      }
-    } catch (e) {
-      debugPrint('Error initializing camera: $e');
-    }
-  }
-
-  void _startRecording() {
-    setState(() {
-      _isRecording = true;
-      _frameCount = 0;
-    });
-
-    // Capture frame every 5 seconds
-    _timer = Timer.periodic(const Duration(seconds: 5), (timer) async {
-      if (_controller != null && _controller!.value.isInitialized) {
-        try {
-          final image = await _controller!.takePicture();
-          setState(() {
-            _frameCount++;
-          });
-          
-          // Here you would send the image to your API
-          debugPrint('Frame captured: ${image.path}');
-          // TODO: Implement API call here
-          
-        } catch (e) {
-          debugPrint('Error capturing frame: $e');
-        }
-      }
-    });
-  }
-
-  void _stopRecording() {
-    _timer?.cancel();
-    setState(() {
-      _isRecording = false;
-    });
-  }
-
-  @override
-  void dispose() {
-    _timer?.cancel();
-    _controller?.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (_controller == null || !_controller!.value.isInitialized) {
-      return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Recording: ${widget.intervieweeName}'),
         backgroundColor: Colors.transparent,
         elevation: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search), 
+            onPressed: null, // Disabled the search action
+          ),
+        ],
       ),
       body: Column(
         children: [
-          Expanded(
-            child: AspectRatio(
-              aspectRatio: _controller!.value.aspectRatio,
-              child: CameraPreview(_controller!),
-            ),
-          ),
           Container(
             padding: const EdgeInsets.all(16),
             child: Column(
@@ -118,7 +40,7 @@ class _RecordingPageState extends State<RecordingPage> {
                   ),
                 const SizedBox(height: 16),
                 ElevatedButton(
-                  onPressed: _isRecording ? _stopRecording : _startRecording,
+                  onPressed: null, // Disabled the button action
                   style: ElevatedButton.styleFrom(
                     backgroundColor: _isRecording ? Colors.red : Colors.blue,
                     padding: const EdgeInsets.symmetric(
@@ -138,4 +60,4 @@ class _RecordingPageState extends State<RecordingPage> {
       ),
     );
   }
-} 
+}
